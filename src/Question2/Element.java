c531e6839f26be962ae09baf53bfde5651c90606
sarabaghani <sarabaghani72@gmail.com>
2023-03-16 02:00:09 +0600

    create abstract class Element with proper fields and methods

---
+++ src/Question2/Element.java
@@ -0,0 +1,27 @@
+package Question2;
+
+public abstract class Element {
+    private String symbol;
+    private int atomicNumber;
+    private double atomicWeight;
+
+    public Element(String symbol, int atomicNumber, double atomicWeight) {
+        this.symbol = symbol;
+        this.atomicNumber = atomicNumber;
+        this.atomicWeight = atomicWeight;
+    }
+
+    public abstract void describeElement();
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getAtomicNumber() {
+        return atomicNumber;
+    }
+
+    public double getAtomicWeight() {
+        return atomicWeight;
+    }
+}
