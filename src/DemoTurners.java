9af85c7f6e81b41f8c61801f26adbf2d8e61adbd
sarabaghani <sarabaghani72@gmail.com>
2023-03-15 20:24:40 +0600

    test different objects

--- src/DemoTurners.java
+++ src/DemoTurners.java
@@ -1,5 +1,13 @@
 public class DemoTurners {
     public static void main(String[] args) {
-
+        Leaf leaf = new Leaf();
+        System.out.print(leaf.getClass().getSimpleName()+": ");
+        leaf.turner();
+        Page page = new Page();
+        System.out.print(page.getClass().getSimpleName()+": ");
+        page.turner();
+        Pancake pancake = new Pancake();
+        System.out.print(pancake.getClass().getSimpleName()+": ");
+        pancake.turner();
     }
 }
