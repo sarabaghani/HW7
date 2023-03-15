754711583c351ed5ee1ff49f06f7891c9bb8f0fc
sarabaghani <sarabaghani72@gmail.com>
2023-03-16 02:56:16 +0600

    make some little changes in output

--- src/Question2/MetalElement.java
+++ src/Question2/MetalElement.java
@@ -7,7 +7,7 @@ public class MetalElement extends Element {
 
     @Override
     public void describeElement() {
-        System.out.println(getSymbol() + " is an element in periodic table which atomic number is "
+        System.out.println("- "+getSymbol() + " is an element in periodic table which atomic number is "
                 + getAtomicNumber() + " and the atomic weight is " + getAtomicWeight());
         System.out.println("This elements is a metal element, " +
                 "which means it's lustrous, malleable, ductile, good conductor of \nheat and electricity. ");
--- src/Question2/NonMetalElement.java
+++ src/Question2/NonMetalElement.java
@@ -7,7 +7,7 @@ public class NonMetalElement extends Element{
 
     @Override
     public void describeElement() {
-        System.out.println(getSymbol()+" is an element in periodic table which atomic number is "
+        System.out.println("- "+getSymbol()+" is an element in periodic table which atomic number is "
                 +getAtomicNumber()+ " and the atomic weight is "+getAtomicWeight());
         System.out.println("This elements is a non-metal element, which means it's a poor conductor of heat and electricity,\n" +
                 "and is not malleable or ductile.");
